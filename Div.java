2dfad3d160a17c866e121e4d810fcb466853396e
abhi <rajabhishek7654@gmail.com>
2025-01-07 11:59:25 +0530

    Div addition functionality

---
+++ Div.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Calculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        int num1 = scanner.nextInt();
+
+        System.out.print("Enter second number: ");
+        int num2 = scanner.nextInt();
+
+     if (num2 != 0) {
+    double quotient = (double) num1 / num2;
+    System.out.println("The quotient is: " + quotient);
+} else {
+    System.out.println("Division by zero is not allowed.");
+}
+
+
+
+    }
+}
